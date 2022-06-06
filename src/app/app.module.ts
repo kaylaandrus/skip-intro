76e22ddeace820565504721810c1b7d365b66cac
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-06-06 13:30:13 -0500

    Added shared component and subcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SharedComponent } from './shared/shared.component';
+import { TvshowComponent } from './shared/tvshow/tvshow.component';
+import { DirectivesComponent } from './shared/directives/directives.component';
+import { HttpComponent } from './shared/http/http.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SharedComponent,
+    TvshowComponent,
+    DirectivesComponent,
+    HttpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

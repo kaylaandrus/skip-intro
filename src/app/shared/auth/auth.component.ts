ebb4ef3b5307e06b6176475b26431b173491700a
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-06-13 15:28:42 -0500

    Added sign in logic

--- src/app/shared/auth/auth.component.ts
+++ src/app/shared/auth/auth.component.ts
@@ -1,7 +1,8 @@
-import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 import { Component, NgModule, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AuthResponseData, AuthService } from './auth.service';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,21 +11,43 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  authObs: Observable<any>;
+  isSignUpMode = true;
+  isLoginMode = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
   }
 
-  isLoginMode = true;
+  errMsg = null;
+
+  authObsrv: Observable<AuthResponseData>;
 
   onSwitchAuthMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onAuthFormSubmit(formObj: NgForm) {
-    console.log('Form Values', formObj.value);
+    if (!formObj.valid) return;
+
+    const { email, password } = formObj.value
+
+    if (this.isLoginMode) {
+      this.authObsrv = this.authService.signIn(email, password);
+    } else {
+      this.authObsrv = this.authService.signUp(email, password);
+    }
+    this.authObsrv.subscribe(
+      (res) => {
+        console.log('Auth Res Success:', res);
+        if (this.errMsg) this.errMsg = null;
+      },
+      (err) => {
+        console.error('Auth Res Error:', err);
+        this.errMsg = err.message;
+      }
+    );
     formObj.reset();
   }
-
 }
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,14 +1,23 @@
 import { HttpClient } from "@angular/common/http"
+import { BehaviorSubject } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Router } from "@angular/router";
 import { Injectable } from "@angular/core"
+
 
 const AUTH_API_KEY = "lb2NG4CiWnUIRQgEbzvcKDeXMyKXCNern1SH67eN";
 const SIGN_UP_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBy1E_D6OgqJ-3c3mmP09rUm6GRxHlqZko";
 const SIGN_IN_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=IzaSyBy1E_D6OgqJ-3c3mmP09rUm6GRxHlqZko";
 
-@Injectable({
+Injectable({
   providedIn: "root",
 })
+
+export interface AuthResponseData {
+  registered?: boolean
+}
 export class AuthService {
+
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
@@ -17,5 +26,15 @@ export class AuthService {
       password,
       returnSecureToken: true,
     });
+  }
+  signIn (email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      SIGN_IN_URL + AUTH_API_KEY,
+      {
+        email,
+        password,
+        returnSecureToken: true,
+      }
+    );
   }
 }

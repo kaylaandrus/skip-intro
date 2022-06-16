5f5badad6c603bd7ff6ad68328c9b44c4d57f487
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-06-16 11:20:59 -0500

    Updated: authguard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { HbomaxComponent } from './what-to-watch/hbomax/hbomax.component';
 import { HuluComponent } from './what-to-watch/hulu/hulu.component';
 import { NetflixComponent } from './what-to-watch/netflix/netflix.component';
 import { WhatToWatchComponent } from './what-to-watch/what-to-watch.component';
+import { AuthGuard } from './shared/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},

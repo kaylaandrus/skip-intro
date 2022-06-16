eaddfd1afb6dd7af9dbc5f9c031c07736a87d4ab
kaylaandrus <kaylam.andrus@gmail.com>
2022-06-16 11:50:10 -0500

    Watchlist edit button

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
-  styleUrls: ['./watchlist.component.css']
+  styleUrls: ['./watchlist.component.css'],
 })
 export class WatchlistComponent implements OnInit {
+  tvShowTest = [
+    'Game of Thrones',
+    'Gilmore Girls',
+    'The Boys',
+    'Stranger Things',
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

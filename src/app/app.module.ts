9e0ea6593f99852ef0196ce47255acb24e7ac4a9
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-06-17 12:59:25 -0500

    Updated routing module with revisions to streaming service components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,11 @@ import { ShowDetailsComponent } from './watchlist/show-details/show-details.comp
 import { WhatToWatchComponent } from './what-to-watch/what-to-watch.component';
 import { ShowResultsComponent } from './what-to-watch/show-results/show-results.component';
 import { ShowSearchComponent } from './what-to-watch/show-search/show-search.component';
-import { NetflixComponent } from './what-to-watch/netflix/netflix.component';
-import { HuluComponent } from './what-to-watch/hulu/hulu.component';
-import { HbomaxComponent } from './what-to-watch/hbomax/hbomax.component';
-import { DisneyplusComponent } from './what-to-watch/disneyplus/disneyplus.component';
-import { AmazonprimeComponent } from './what-to-watch/amazonprime/amazonprime.component';
+import { NetflixComponent } from './what-to-watch/streaming-services/netflix/netflix.component';
+import { HuluComponent } from './what-to-watch/streaming-services/hulu/hulu.component';
+import { HbomaxComponent } from './what-to-watch/streaming-services/hbomax/hbomax.component';
+import { DisneyplusComponent } from './what-to-watch/streaming-services/disneyplus/disneyplus.component';
+import { AmazonprimeComponent } from './what-to-watch/streaming-services/amazonprime/amazonprime.component';
 import { SharedModule } from './shared/shared.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';

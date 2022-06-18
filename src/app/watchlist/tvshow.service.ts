6b861cf0be8e7c894f1789d8d15f1a803ce03dc7
kaylaandrus <kaylam.andrus@gmail.com>
2022-06-18 11:36:01 -0500

    tvshow service

--- src/app/watchlist/tvshow.service.ts
+++ src/app/watchlist/tvshow.service.ts
@@ -6,14 +6,30 @@ import { Show } from '../shared/tvshow/tvshow.model';
 })
 export class TvshowService {
   selectedTvshow = new Subject<Show>();
-  watchlistChanged = new Subject<Show[]>();
+  tvshowsChanged = new Subject<Show[]>();
   private myTvshows: Show[] = [];
 
+  // Save show to watchlist
   saveShowToWatchlist(show: Show) {
     this.myTvshows.push(show);
     this.selectedTvshow.next(show);
-    this.watchlistChanged.next(this.myTvshows.slice());
+    this.tvshowsChanged.next(this.myTvshows.slice());
+  }
+  // Watch many
+  getTvshows() {
+    return this.myTvshows.slice();
+  }
+  // Watch one
+  getTvshow(idx: number) {
+    return this.myTvshows.slice()[idx];
   }
 
+  // Delete show
+  deleteShowFromMyTvshows(idx: number) {
+    if (idx === -1) return;
+    this.selectedTvshow.next(this.myTvshows[idx]);
+    this.myTvshows.splice(idx, 1);
+    this.tvshowsChanged.next(this.myTvshows.slice());
+  }
   constructor() {}
 }

a014aa34818a179d18e43a55f758b3f04368cb20
kaylaandrus <kaylam.andrus@gmail.com>
2022-06-19 09:55:55 -0500

    updated http.service

--- src/app/shared/http/http.service.ts
+++ src/app/shared/http/http.service.ts
@@ -1,31 +1,40 @@
-import { tap } from "rxjs";
-import { Injectable, Pipe } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { WatchlistService } from "src/app/watchlist/watchlist.service";
-import { Show } from "../show/show.model";
-
-const FIREBASE_DB_URL = "https://skip-intro-7b47f-default-rtdb.firebaseio.com/";
+import { tap } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { WatchlistService } from 'src/app/watchlist/watchlist.service';
+import { Show } from '../show/show.model';
 
 @Injectable({
-  providedIn: "root",})
-
+  providedIn: 'root',
+})
 export class HTTPService {
+  FIREBASE_DB_URL = 'https://skip-intro-7b47f-default-rtdb.firebaseio.com/';
 
   constructor(
     private http: HttpClient,
     private watchlistService: WatchlistService
-    ) {}
-    saveShowsToFirebase() {
-      const saveShows = this.watchlistService.getWatchlistShows();
+  ) {}
 
-      this.http.put(FIREBASE_DB_URL, saveShows).subscribe((res) => {
-        console.log("Firebase DB Response:", res);
-      });
-    }
-    // FIGURE OUT WHY THIS DOESNT WORK
-    // fetchShowsFromFirebase() {
-      //return this.http.get(FIREBASE_DB_URL, {}).pipe(tap((show: Shows[]) => {
-          //this.watchlistService.setWatchlistShows(shows);
-        //})
-    //);
+  saveShowsToFirebase() {
+    const saveShows = this.watchlistService.getWatchlistShows();
+
+    this.http.put(this.FIREBASE_DB_URL, saveShows).subscribe((res) => {
+      console.log('Firebase DB Response:', res);
+    });
   }
+
+  // *METHOD* - Fetch books from Firebase DB
+  fetchBooksFromFirebase() {
+    return this.http.get(this.FIREBASE_DB_URL, {}).pipe(
+      tap((shows: Show[]) => {
+        this.watchlistService.setWatchlistShows(shows);
+      })
+    );
+  }
+  // FIGURE OUT WHY THIS DOESNT WORK
+  // fetchShowsFromFirebase() {
+  //return this.http.get(FIREBASE_DB_URL, {}).pipe(tap((show: Shows[]) => {
+  //this.watchlistService.setWatchlistShows(shows);
+  //})
+  //);
+}

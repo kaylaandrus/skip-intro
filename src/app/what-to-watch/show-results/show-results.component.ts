3484d19864131f13c5d68a3e353cf2f2847e6dcf
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-06-18 19:44:17 -0500

    Injected watchlist service into book results and created functional button to save shows

--- src/app/what-to-watch/show-results/show-results.component.ts
+++ src/app/what-to-watch/show-results/show-results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Show } from 'src/app/shared/show/show.model';
 import { StreamingLibraryService } from '../streaming-library.service';
+import { WatchlistService } from 'src/app/watchlist/watchlist.service';
 
 @Component({
   selector: 'app-show-results',
@@ -11,9 +12,15 @@ export class ShowResultsComponent implements OnInit {
 
   allShows: Show[] = [];
 
-  constructor(private streamingLibraryService: StreamingLibraryService) { }
+  constructor(
+    private streamingLibraryService: StreamingLibraryService,
+    private watchlistService: WatchlistService
+    ) { }
 
   ngOnInit(): void {
     this.allShows = this.streamingLibraryService.getShows();
+  }
+  onSaveShow(show: Show) {
+    return this.watchlistService.saveShowToWatchlist(show);
   }
 }

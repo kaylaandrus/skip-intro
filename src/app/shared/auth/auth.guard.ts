6ae4ba7f3c231da3de37214bcc3f6c6327913bde
kaylaandrus <kaylam.andrus@gmail.com>
2022-06-19 11:25:52 -0500

    auth service debugging

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -1,20 +1,27 @@
-import { AuthService } from "./auth.service";
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
-import { map, take } from "rxjs";
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { map, take } from 'rxjs';
 
-@Injectable({ providedIn: "root"})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.authService.currentUser.pipe(
       take(1),
-      map(user => {
+      map((user) => {
         const isAuth = !!user;
 
-        return isAuth ? true : this.router.createUrlTree(["auth"]);
+        return isAuth ? true : this.router.createUrlTree(['auth']);
       })
     );
   }
 }
+
+// are activatedroutesnapshot and routerstatesnapshot neccesary?

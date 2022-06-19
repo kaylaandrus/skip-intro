9dda23ef8a55185ffc236c6802dce7326b835a19
kaylaandrus <kaylam.andrus@gmail.com>
2022-06-19 13:06:33 -0500

    auth interceptor service.ts minor changes

--- src/app/shared/auth/auth-interceptor.service.ts
+++ src/app/shared/auth/auth-interceptor.service.ts
@@ -1,20 +1,25 @@
-import { AuthService } from "./auth.service";
-import { Injectable } from "@angular/core";
-import { HttpHandler, HttpInterceptor, HttpParams, HttpRequest } from "@angular/common/http";
-import { exhaustMap, take } from "rxjs";
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
+import { exhaustMap, take } from 'rxjs';
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: 'root' })
 export class AuthInterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return this.authService.currentUser.pipe(
-      take (1),
-      exhaustMap((User) => {
-        if (!User) return next.handle(req);
+      take(1),
+      exhaustMap((user) => {
+        if (!user) return next.handle(req);
 
         const modifiedReq = req.clone({
-          params: new HttpParams().set("auth", User.token),
+          params: new HttpParams().set('auth', user.token),
         });
         return next.handle(modifiedReq);
       })

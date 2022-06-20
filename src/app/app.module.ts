79638db2683ea28a657a77d110bf7281674066cd
krsgarland <97922634+krsgarland@users.noreply.github.com>
2022-06-19 23:59:37 -0500

    Fixed imports on master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
+import { WatchlistModule } from './watchlist/watchlist.module';
+import { StreamingLibraryModule } from './what-to-watch/streaming-library.module';
 import { SharedModule } from './shared/shared.module';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+
 
 //import { ServiceWorkerModule } from '@angular/service-worker';
 //import { environment } from '../environments/environment';
@@ -20,14 +21,14 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    AuthModule,
    // ServiceWorkerModule.register('ngsw-worker.js', {
       //enabled: environment.production,
       //registrationStrategy: 'registerWhenStable30000'
--- src/app/what-to-watch/what-to-watch.component.ts
+++ src/app/what-to-watch/what-to-watch.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ShowSearchComponent } from './show-search/show-search.component';
 
 @Component({
   selector: 'app-what-to-watch',

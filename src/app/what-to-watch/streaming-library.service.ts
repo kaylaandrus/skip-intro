4f39f76424eda3e9e078db921c3af468153dcde2
kaylaandrus <kaylam.andrus@gmail.com>
2022-06-22 22:02:57 -0500

    behold a functioning API

--- src/app/what-to-watch/streaming-library.service.ts
+++ src/app/what-to-watch/streaming-library.service.ts
@@ -1,39 +1,40 @@
-import { Injectable, EventEmitter } from "@angular/core";
-import { Show } from "../shared/show/show.model";
-import { HttpClient } from "@angular/common/http";
+import { Injectable, EventEmitter } from '@angular/core';
+import { Show } from '../shared/show/show.model';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
-
 export class StreamingLibraryService {
-  showListChanged = new EventEmitter();
-
-  constructor(private http: HttpClient){}
+  showListChanged = new Subject<Show[]>();
 
   private allShows: Show[] = [];
+  constructor(private http: HttpClient) {}
 
   fetchShows() {
     return this.allShows.slice();
   }
   fetchShowsFromAPI(query: string) {
-    const formattedQuery = query
-    .split("")
-    .join ("+")
-    .toLowerCase();
+    const formattedQuery = query.split('').join('%20').toLowerCase();
 
     this.http
-    .get(`https://api.watchmode.com/v1/list-titles/?apiKey=lb2NG4CiWnUIRQgEbzvcKDeXMyKXCNern1SH67eN${formattedQuery}`)
-    .subscribe((searchResults: any) => {
-      this.saveShowsToGlobalArray(searchResults.docs.slice(0, 10));
-    });
+      .get(
+        `https://api.watchmode.com/v1/search/?apiKey=lb2NG4CiWnUIRQgEbzvcKDeXMyKXCNern1SH67eN&search_field=name&search_value=${query}`
+      )
+      .subscribe((searchResults: any) => {
+        console.log(searchResults);
+        this.saveShowsToGlobalArray(searchResults.title_results);
+      });
   }
   saveShowsToGlobalArray(shows) {
-    shows.forEach(show => {
+    shows.forEach((show) => {
+      console.log(show);
+
       const simpleShow = new Show(
         show.name,
-        show.source_ids ? show.source_ids[0] : "unknown",
-        "unknown",
+        show.source_ids ? show.source_ids[0] : 'unknown',
+        'unknown',
         'https://i.ibb.co/NFrzxpW/91168225-5507-4380-B65-D-C52-E86472299.png'
       );
       this.allShows.push(simpleShow);
